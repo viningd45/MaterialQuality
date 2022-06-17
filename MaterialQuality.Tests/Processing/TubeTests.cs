1162e2f37439edaf1d12d3812196842cce1c8f50
Daniel Vining <viningd45@gmail.com>
2022-06-16 23:54:32 -0500

    add testing project

---
+++ MaterialQuality.Tests/Processing/TubeTests.cs
@@ -0,0 +1,34 @@
+﻿using MaterialQuality.Processing.Entities;
+using MaterialQuality.Processing.Constants;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaterialQuality.Tests.Processing
+{
+    [TestFixture]
+    public class TubeTests
+    {
+        Tolerances tolerances;
+        Tube tube;
+
+        [SetUp]
+        public void SetUp()
+        {
+            tolerances = new Tolerances();
+            tube = new Tube();
+        }
+
+        [Test]
+        public void DiameterOverMax_GivesQualityCode()
+        {
+            tolerances.OuterDiameterMax = 139.1;
+            tube.OuterDiameter = 140;
+
+            tube.EvaluateQuality(tolerances);
+
+            Assert.IsTrue(tube.DefectCodes.Contains(QualityCode.TubeQualityOuterDiameterPlus));
+        }
+    }
+}
